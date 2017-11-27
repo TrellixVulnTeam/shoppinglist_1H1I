d79cc3b518887cd77598bc2a682b438c81738ab6
Samuvel P <samuvelpandian@gmail.com>
2017-11-27 21:53:38 +0530

    Designed login Screen

---
+++ app/src/main/java/com/pandian/samuvel/shoppinglist/Activities/Auth/CreateAccountActivity.java
@@ -0,0 +1,19 @@
+package com.pandian.samuvel.shoppinglist.Activities.Auth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.pandian.samuvel.shoppinglist.R;
+
+import jp.wasabeef.blurry.Blurry;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+    }
+}
---
+++ app/src/main/java/com/pandian/samuvel/shoppinglist/Activities/Auth/SignInActivity.java
@@ -0,0 +1,15 @@
+package com.pandian.samuvel.shoppinglist.Activities.Auth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pandian.samuvel.shoppinglist.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
